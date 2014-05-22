3f869462db6f7a55544fcc8cc938cfa80d262ccf
James Rowley <pixelfelon@gmail.com>
2014-05-22 16:41:52 -0700

    Working Model
    Uses square root of sine.

--- js/led.js
+++ js/led.js
@@ -1,1 +1,22 @@
 /*RGB-Tester Scripts*/
+function init(){
+led1 = document.getElementById("led1")
+led2 = document.getElementById("led2")
+led3 = document.getElementById("led3")
+led4 = document.getElementById("led4")
+led5 = document.getElementById("led5")
+led6 = document.getElementById("led6")
+led7 = document.getElementById("led7")
+led8 = document.getElementById("led8")
+}
+
+function tick(){
+led1.style.color="rgb(30,30,30)";
+led2.style.color="rgb(30,30,30)";
+led3.style.color="rgb(30,30,30)";
+led4.style.color="rgb(30,30,30)";
+led5.style.color="rgb(30,30,30)";
+led6.style.color="rgb(30,30,30)";
+led7.style.color="rgb(30,30,30)";
+led8.style.color="rgb(30,30,30)";
+}
